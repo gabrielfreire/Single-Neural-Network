4ac43475982edd5a0d33bb775c48d0d4b17e71bd
Gabriel Freire <Gabriel Freire>
2017-04-10 12:44:26 +0100

    Implemented the Activation Function

--- src/ActivationFunction.java
+++ src/ActivationFunction.java
@@ -2,4 +2,11 @@
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class ActivationFunction {
+
+    public static int stepFunction(float activation){
+        if(activation >= 1){
+            return 1;
+        }
+        return 0;
+    }
 }
