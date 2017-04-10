47c77f5abb3155d9f79d354245ad15244cc2814b
Gabriel Freire <Gabriel Freire>
2017-04-10 12:55:55 +0100

    Perceptron implementation and training algorithm

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -2,4 +2,54 @@
  * Created by Gabriel Freire on 10/04/2017.
  */
 public class Perceptron {
+
+    private float[] weights;
+    private float[][] input;
+    private float[] output;
+    private int numOfWeights;
+
+    public Perceptron(float[][] input, float[] output){
+        this.input = input;
+        this.output = output;
+        this.numOfWeights = input[0].length;
+        this.weights = new float[numOfWeights];
+
+        initializeWeights();
+    }
+
+    private void initializeWeights() {
+        for(int i = 0; i < numOfWeights; i++){
+            weights[i] = 0;
+        }
+    }
+
+    public void train(float learningRate, int epochs){
+
+        float totalError = 1;
+        while(totalError != 0){
+            totalError = 0;
+            for(int i = 0; i < output.length; i++){
+                //calculate output and error
+                float calculatedOuput = calculateOutput(input[i]);
+                float error = Math.abs(output[i] - calculatedOuput);
+
+                totalError += error;
+
+                //Calculate the weights according to the error and learning rate
+                for(int j = 0; j < numOfWeights; j++){
+                    weights[j] = weights[j] + learningRate * input[i][j] * error;
+                }
+            }
+            System.out.println("Keep on training the network, error is: " + totalError);
+        }
+
+    }
+
+    public float calculateOutput(float[] input){
+        float sum = 0f;
+        for(int i = 0; i < input.length; i++){
+            sum += weights[i] * input[i];
+        }
+        return ActivationFunction.stepFunction(sum);
+    }
 }
